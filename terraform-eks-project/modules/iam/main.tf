e41411a8aec7b382d3163530edd8213a58c21472
iam-vihaan <asathyavihaan@gmail.com>
2025-07-04 11:47:33 +0530

    Create main.tf

---
+++ terraform-eks-project/modules/iam/main.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_policy" "alb_controller" {
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  path        = "/"
+  description = "IAM policy for AWS Load Balancer Controller"
+  policy      = file("${path.module}/alb-policy.json")
+}
+
+resource "aws_iam_role" "alb_controller" {
+  name = "alb-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "alb_attach" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = aws_iam_policy.alb_controller.arn
+}
