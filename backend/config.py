b6b6ba86275b78b1dcc0be87fa32e26a49a539b3
iam-vihaan <asathyavihaan@gmail.com>
2025-07-04 10:49:41 +0530

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
